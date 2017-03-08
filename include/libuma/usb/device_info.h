175fabe85bf08fafd5a628fb3a683b56ea794005
Theowhy <theowhy@shizoku.fr>
2017-03-08 23:51:27 +0100

    Add USB definition
     This contains usb product_id and vendor_id

---
+++ include/libuma/usb/device_info.h
@@ -0,0 +1,10 @@
+
+#ifndef UMA_DEVICE_USB_DEFINITION
+#define UMA_DEVICE_USB_DEFINITION
+
+#define MOOLTIPASS_VENDOR_ID  0x16D0              // Vendor ID (from MCS)
+#define MOOLTIPASS_PRODUCT_ID 0x09A0              // Product ID (from MCS)
+#define STR_MANUFACTURER    L"SE"               // Manufacturer string
+#define STR_PRODUCT         L"Mooltipass"       // Product string
+
+#endif //UMA_DEVICE_USB_DEFINITION
