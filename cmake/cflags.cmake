6f00ca6473f138194cc0de668aff1db0ffdbd25a
Theowhy <theowhy@shizoku.fr>
2017-05-21 13:02:11 +0200

    Add basic compilation flags for warnings

---
+++ cmake/cflags.cmake
@@ -0,0 +1,25 @@
+include(CheckCCompilerFlag)
+
+function(add_cflag flag)
+	string(REPLACE "-" "_" flagclean ${flag})
+	string(REPLACE "=" "_" flagclean ${flagclean})
+	string(REPLACE "+" "_" flagclean ${flagclean})
+	string(REPLACE "," "_" flagclean ${flagclean})
+	string(TOUPPER ${flagclean} flagclean)
+
+	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flag}" PARENT_SCOPE)
+endfunction()
+
+function(add_cflag_if_supported flag)
+	string(REPLACE "-" "_" flagclean ${flag})
+	string(REPLACE "=" "_" flagclean ${flagclean})
+	string(REPLACE "+" "_" flagclean ${flagclean})
+	string(REPLACE "," "_" flagclean ${flagclean})
+	string(TOUPPER ${flagclean} flagclean)
+
+	check_c_compiler_flag(${flag} C_SUPPORTS${flagclean})
+
+	if (C_SUPPORTS${flagclean})
+		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flag}" PARENT_SCOPE)
+	endif()
+endfunction()
