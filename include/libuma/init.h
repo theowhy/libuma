81f0008554abddadc42bfb5aaa0f3da7872c7443
Theowhy <theowhy@shizoku.fr>
2017-07-01 00:47:57 +0200

    Adds documentation for init.h

--- include/libuma/init.h
+++ include/libuma/init.h
@@ -7,7 +7,20 @@
 extern "C" {
 #endif
 
+/*!
+ * Initialize library. A call to this function is required before using any other library's function.
+ * After using the library, a clean up must be made using \ref uma_shutdown
+ *
+ * \return UMA_SUCCESS on success or UMA_ERROR_USB_INIT if the USB sub system cannot be initialized
+ */
 uma_ret_code uma_init();
+
+/*!
+ * \brief Cleans everything up.
+ *
+ * After using the library's functions, calling this function will deinitialized USB sub system and clean library's internals<.
+ *
+ */
 void uma_shutdown();
 
 #ifdef __cplusplus
