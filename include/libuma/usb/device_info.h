65146b34c6f9a20d63441042c2fa37bb016fc567
Theowhy <theowhy@shizoku.fr>
2017-07-01 00:04:49 +0200

    Remove useless informations in USB device definition

--- include/libuma/usb/device_info.h
+++ include/libuma/usb/device_info.h
@@ -2,9 +2,7 @@
 #ifndef UMA_DEVICE_USB_DEFINITION
 #define UMA_DEVICE_USB_DEFINITION
 
-#define MOOLTIPASS_VENDOR_ID  0x16D0              // Vendor ID (from MCS)
-#define MOOLTIPASS_PRODUCT_ID 0x09A0              // Product ID (from MCS)
-#define STR_MANUFACTURER    L"SE"               // Manufacturer string
-#define STR_PRODUCT         L"Mooltipass"       // Product string
+#define MOOLTIPASS_VENDOR_ID  0x16D0  //< Vendor ID
+#define MOOLTIPASS_PRODUCT_ID 0x09A0  //< Product ID (from MCS)
 
 #endif //UMA_DEVICE_USB_DEFINITION
